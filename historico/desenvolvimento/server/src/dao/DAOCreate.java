3640a4be7bdab9bc185fa354050e9649d99cd829
addller <addllerr@gmail.com>
2018-05-30 02:44:49 -0300

    Criado método que registra a imagem de perfil e metodos que ajustam o tamanho da imagem

--- historico/desenvolvimento/server/src/dao/DAOCreate.java
+++ historico/desenvolvimento/server/src/dao/DAOCreate.java
@@ -7,10 +7,17 @@ package dao;
 
 import acesso.Login;
 import cadastro.Cadastro;
+import ferramentas.EscalaImagem;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.imageio.ImageIO;
 import perfil.Membro;
 
 /**
@@ -26,7 +33,7 @@ public class DAOCreate extends DAO {
     public ResultSet registrarMembro(Membro membro) throws SQLException {
         Cadastro cadastro = membro.getCadastro();
         Login login = membro.getLogin();
-        System.out.println(cadastro.getSexo()+" "+((int)cadastro.getSexo()));
+        System.out.println(cadastro.getSexo() + " " + ((int) cadastro.getSexo()));
         query("select id from registrarMembro(?,?,?,?,?,?) as id");
         encadear(new int[]{1, 4, 6}, cadastro.getNome(), login.getLoginName(), login.getEmail());
         statement.setDate(2, cadastro.getNascimento());
@@ -34,6 +41,40 @@ public class DAOCreate extends DAO {
         statement.setBytes(5, login.getPass());
         return executeQuery(true);
 
+    }
+
+    public ResultSet registrarImagemPerfil(Membro membro, EscalaImagem escala) throws SQLException, IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(ajustarImagem(membro.getImgMembro(), escala), "jpg", bos);
+        query("select id from registrarImagemPerfil(?,?,?) as id");
+        statement.setLong(1, membro.getId());
+        statement.setBytes(2, bos.toByteArray());
+        statement.setString(3, escala.toString());
+        return executeQuery(true);
+    }
+
+    private BufferedImage ajustarImagem(BufferedImage imagem, EscalaImagem escala) {
+        int largura = imagem.getWidth(),
+                altura = imagem.getHeight();
+        double impPercentualPequena = (double) 100 / largura,
+                imgPercentualMedia = (double) 500 / largura;
+        switch (escala) {
+            case PEQUENA:
+                return ajustarImagem(imagem, largura * impPercentualPequena, altura * impPercentualPequena);
+            case MEDIA:
+                return ajustarImagem(imagem, largura * imgPercentualMedia, altura * imgPercentualMedia);
+            default:
+                return imagem;
+        }
+    }
+
+    private BufferedImage ajustarImagem(BufferedImage image, double largura, double altura) {
+        int xLargura = (int) largura,
+                yAltura = (int) altura;
+        BufferedImage novaImagem = new BufferedImage(xLargura, yAltura, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = (Graphics2D) novaImagem.getGraphics();
+        g.drawImage(image, 0, 0, xLargura, yAltura, null);
+        return novaImagem;
     }
 
 }
